df7071d7936bdaf53460fc535c83287d38f6d753
luojin <luojin_mail@163.com>
2019-05-03 19:57:08 +0800

    完成dubbo最基础例子搭建

--- dubbo-consumer/src/main/java/com/lj/controller/DubboConsumerController.java
+++ dubbo-consumer/src/main/java/com/lj/controller/DubboConsumerController.java
@@ -1,9 +1,9 @@
 package com.lj.controller;
 
+import com.lj.service.DubboProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import service.DubboProviderService;
 
 @RestController
 public class DubboConsumerController {
